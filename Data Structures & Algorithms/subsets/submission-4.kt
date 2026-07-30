8429a512fcb02989d62f8214edca5ae8949100bb
Ashish Ekka <ashishekka329@gmail.com>
2026-07-30 18:23:56 +0530

    Add: subsets - submission-4

---
+++ Data Structures & Algorithms/subsets/submission-4.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun subsets(nums: IntArray): List<List<Int>> {
+        val result = mutableListOf<List<Int>>()
+
+        fun dfs(index: Int, path: MutableList<Int>) {
+            if (index == nums.size) {
+                result.add(path.toList())
+                return
+            }
+
+            path.add(nums[index])
+            dfs(index + 1, path)
+
+            path.removeLast()
+            dfs(index + 1, path)
+        }
+        dfs(0, mutableListOf<Int>())
+
+        return result
+    }
+}
