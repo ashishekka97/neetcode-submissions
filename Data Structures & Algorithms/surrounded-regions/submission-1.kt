60e452d4991c70195bfdd8432b58a7854037c420
Ashish Ekka <ashishekka329@gmail.com>
2026-08-07 00:34:22 +0530

    Add: surrounded-regions - submission-1

---
+++ Data Structures & Algorithms/surrounded-regions/submission-1.kt
@@ -0,0 +1,39 @@
+class Solution {
+    fun solve(board: Array<CharArray>) {
+        val rows = board.size
+        val cols = board[0].size
+
+        val dirs = intArrayOf(0, 1, 0, -1, 0)
+
+        fun dfs(row: Int, col: Int) {
+            if (row !in 0 until rows || col !in 0 until cols) return
+            if (board[row][col] != 'O') return
+
+            board[row][col] = 'T'
+
+            for (d in 0 until 4) {
+                dfs(row + dirs[d], col + dirs[d + 1])
+            }
+        }
+
+        for (col in 0 until cols) {
+            if (board[0][col] == 'O') dfs(0, col)
+            if (board[rows - 1][col] == 'O') dfs(rows - 1, col)
+        }
+
+        for (row in 0 until rows) {
+            if (board[row][0] == 'O') dfs(row, 0)
+            if (board[row][cols - 1] == 'O') dfs(row, cols - 1)
+        }
+
+        for (row in 0 until rows) {
+            for (col in 0 until cols) {
+                if (board[row][col] == 'O') {
+                    board[row][col] = 'X'
+                } else if (board[row][col] == 'T') {
+                    board[row][col] = 'O'
+                }
+            }
+        }
+    }
+}
